535149658f0a8efa5fa15a824e751d5c89363677
zhuliang <lezon1995@163.com>
2025-11-09 12:31:03 +0800

    fotmat

--- Packages/com.zhourui.myframework/Runtime/Frame_Game/GameFramework/FrameSystem.cs
+++ Packages/com.zhourui.myframework/Runtime/Frame_Game/GameFramework/FrameSystem.cs
@@ -1,11 +1,25 @@
-﻿
-// 系统组件基类,一般都是管理器
+﻿// 系统组件基类,一般都是管理器
 public class FrameSystem
 {
-	public virtual void init() { }
-	public virtual void update(float elapsedTime) { }
-	// 即将销毁时调用,退出程序时会先调用一次全部系统的即将销毁,再全部调用一次销毁
-	public virtual void willDestroy() { }
-	public virtual void destroy(){}
-	public string getName() { return GetType().ToString(); }
+    public virtual void init()
+    {
+    }
+
+    public virtual void update(float elapsedTime)
+    {
+    }
+
+    // 即将销毁时调用,退出程序时会先调用一次全部系统的即将销毁,再全部调用一次销毁
+    public virtual void willDestroy()
+    {
+    }
+
+    public virtual void destroy()
+    {
+    }
+
+    public string getName()
+    {
+        return GetType().ToString();
+    }
 }
