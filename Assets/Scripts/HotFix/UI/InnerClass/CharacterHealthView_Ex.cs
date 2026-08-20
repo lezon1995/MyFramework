e4f9132de4186aeec2125d33a51ba0317aa4229e
zhuliang <lezon1995@163.com>
2026-08-20 17:31:05 +0800

    feat: 新增可以远程攻击的AI，新增OverlayMenu中的玩家信息

---
+++ Assets/Scripts/HotFix/UI/InnerClass/CharacterHealthView_Ex.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MoreMountains;
+
+public partial class CharacterHealthView
+{
+    public void SetHealth(int cur, int max)
+    {
+        curExp.setText(cur);
+        maxExp.setText(max);
+        expBar.setFillPercent(cur / (float)max);
+    }
+}
---
+++ Assets/Scripts/HotFix/UI/InnerClass/CharacterStatsView_Ex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoreMountains;
+
+public partial class CharacterStatsView
+{
+    public void BuildPlayerStats<TData>(List<TData> dataList, Action<PlayerStatItem, TData> onBuild)
+    {
+        PlayerStatItemPool.newItemList(dataList, onBuild);
+    }
+}
